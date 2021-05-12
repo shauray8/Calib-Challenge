5c49b0ed0acee63c5beb754500cf9d486f07408e
Shauray <shauray9@gmail.com>
2021-05-12 16:16:02 +0530

    another architecture for test

---
+++ calib/GMA.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+
+class GMA(nn.Module):
+    def __init__(self):
+        super(GMA, self).__init__()
+        pass
+
+    def forward(self, x):
+        pass
+
+
+
+if __name__ == "__main__":
+    pass
+
