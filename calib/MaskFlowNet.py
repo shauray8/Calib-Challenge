6d96f823451cb3ef94238aedc0430239039fa3d7
Shauray <shauray9@gmail.com>
2021-05-12 14:05:26 +0530

    maskflo

---
+++ calib/MaskFlowNet.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import numoy as np
+
+
+class MaskFlow(nn.Module):
+    def __init__(self):
+        super(MaskFlow, self).__init__()
+        pass
+
+    def forward(self, x):
+        pass
