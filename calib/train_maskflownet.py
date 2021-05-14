7efabeb1030e132120a3b7ce176f823301acfbd1
Shauray <shauray9@gmail.com>
2021-05-14 23:18:22 +0530

    bored added some arg parsers

--- calib/train_maskflownet.py
+++ calib/train_maskflownet.py
@@ -1,10 +1,15 @@
+import argparse
+import os
+import time
+
 import torch
-import torch.nn as nn
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2
-import PIL
-import torchvision 
+import torch.nn.functional as F
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import datetime
 from torch.utils.tensorboard import SummaryWriter
 
 from FlownetCorr import *
@@ -17,4 +22,78 @@ def callable():
     return kwargs
 
 
+
+parser = argparse.ArgumentParser(description='PyTorch FlowNet Training on several datasets',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('--dataset', metavar='DATASET', default='flying_chairs',
+                    choices=dataset_names,
+                    help='dataset type : ' +
+                    ' | '.join(dataset_names))
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-s', '--split-file', default=None, type=str,
+                   help='test-val split file')
+group.add_argument('--split-value', default=0.8, type=float,
+                   help='test-val split proportion between 0 (only test) and 1 (only train), '
+                        'will be overwritten if a split file is set')
+parser.add_argument('--arch', '-a', metavar='ARCH', default='flownets',
+                    choices=model_names,
+                    help='model architecture, overwritten if pretrained is specified: ' +
+                    ' | '.join(model_names))
+parser.add_argument('--solver', default='adam',choices=['adam','sgd'],
+                    help='solver algorithms')
+parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
+                    help='number of data loading workers')
+parser.add_argument('--epochs', default=300, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('--epoch-size', default=1000, type=int, metavar='N',
+                    help='manual epoch size (will match dataset size if set to 0)')
+parser.add_argument('-b', '--batch-size', default=8, type=int,
+                    metavar='N', help='mini-batch size')
+parser.add_argument('--lr', '--learning-rate', default=0.0001, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum for sgd, alpha parameter for adam')
+parser.add_argument('--beta', default=0.999, type=float, metavar='M',
+                    help='beta parameter for adam')
+parser.add_argument('--weight-decay', '--wd', default=4e-4, type=float,
+                    metavar='W', help='weight decay')
+parser.add_argument('--bias-decay', default=0, type=float,
+                    metavar='B', help='bias decay')
+parser.add_argument('--multiscale-weights', '-w', default=[0.005,0.01,0.02,0.08,0.32], type=float, nargs=5,
+                    help='training weight for each scale, from highest resolution (flow2) to lowest (flow6)',
+                    metavar=('W2', 'W3', 'W4', 'W5', 'W6'))
+parser.add_argument('--sparse', action='store_true',
+                    help='look for NaNs in target flow when computing EPE, avoid if flow is garantied to be dense,'
+                    'automatically seleted when choosing a KITTIdataset')
+parser.add_argument('--print-freq', '-p', default=10, type=int,
+                    metavar='N', help='print frequency')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained', dest='pretrained', default=None,
+                    help='path to pre-trained model')
+parser.add_argument('--no-date', action='store_true',
+                    help='don\'t append date timestamp to folder' )
+parser.add_argument('--div-flow', default=20,
+                    help='value by which flow will be divided. Original value is 20 but 1 with batchNorm gives good results')
+parser.add_argument('--milestones', default=[100,150,200], metavar='N', nargs='*', help='epochs at which learning rate is divided by 2')
+
+best_EPE = -1
+n_iter = 0
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def main():
+    global args, best_EPE
+    args = parser.parse_args()
+    save_path = '{},{},{}epochs{},b{},lr{}'.format(
+        args.arch,
+        args.solver,
+        args.epochs,
+        ',epochSize'+str(args.epoch_size) if args.epoch_size > 0 else '',
+        args.batch_size,
+        args.lr)
 
