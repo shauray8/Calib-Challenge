5292e8bd8f3e35b0075798964fd22496c57a8ce0
Shauray <shauray9@gmail.com>
2021-05-18 12:53:05 +0530

    Update utility

--- calib/utils.py
+++ calib/utils.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import cv2
 import os
 import torch
+import shutil
 
 import random
 import numpy as np
@@ -88,6 +89,35 @@ class RandomTranslate(object):
         target[:,:,1] += th
 
         return inputs, target
+
+
+def save_checkpoint(state, is_best, save_path, filename='checkpoint.pth.tar'):
+    torch.save(state, os.path.join(save_path,filename))
+    if is_best:
+        shutil.copyfile(os.path.join(save_path,filename), os.path.join(save_path,'model_best.pth.tar'))
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __repr__(self):
+        return '{:.3f} ({:.3f})'.format(self.val, self.avg)
+
 
 
 ## ---------------- For Global Motion Aggregation ---------------- ##
