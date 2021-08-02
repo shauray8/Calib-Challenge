3d649556dfb2e85944fb655026e3fc22cfea6dbe
Shauray <shauray9@gmail.com>
2021-08-02 23:42:28 +0530

    pilotne

---
+++ calib/pilotnet.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+
+
+
+if __name__ == "__main__":
+    print(model)
+    ## pilotnet architecture for pred yaw and pitch
+    ## and this time I am thinking of training both yaw and pitch seperatly
