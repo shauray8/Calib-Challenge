fa1be0ae63d1f52cea96ce3807e5edb94a1e7a1e
Shauray <shauray9@gmail.com>
2021-08-05 03:35:28 +0530

    some work in the architecture

--- U-net2_segnets/__init__.py
+++ U-net2_segnets/__init__.py
@@ -1,2 +1,5 @@
 ## segmenting frames from a drive this will help me figure out where to drive and where not to
+## I am planning to use this on other stuff as well like background removers and stuff
+
+from model import Unet_square
 
