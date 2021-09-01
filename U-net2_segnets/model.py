bfb400d0d72c5f2a57c3b6cdf5a529c0bda5febc
Shauray <shauray9@gmail.com>
2021-09-01 16:19:47 +0530

    changing the architecture

--- U-net2_segnets/model.py
+++ U-net2_segnets/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 # --------------- U square Net architecture here the research paper https://arxiv.org/pdf/2005.09007v2.pdf --------------- #
 
@@ -9,38 +9,55 @@ class Unet_square(nn.Module):
         super(Unet_square, self).__init__()
         pass
 
-    def green_block(self):
+    def _upsample(self):
         pass
 
-    def blue_block(self):
+    
+class Green_block(nn.Module):
+    def __init__(self, in_channel=3, out_channel=3, dirate=1):
+        super(Green_block, self).__init__()
+
+        self.conv1_g = nn.Conv2d(in_channel, out_channel, 3, padding=dirate, dilation=dirate)
+        self.bn_g = nn.BatchNorm2d(out_channel)
+        self.relu_g = nn.Relu(inplace=True)
+
+    def forward(self, x):
+
+        return self.relu_g(self.bn_g(self.conv_g(x)))
+
+
+class En_De_1(nn.Module):    
+    def __init__(self):
+        super(En_De_1, self).__init__()
         pass
 
-    def solid_white_block(self):
+    def forward(self, x):
         pass
 
-    def dashed_white_block(self):
+class En_De_2(nn.module):
+    def __init__(self):
+        super(En_De_2, self).__init__()
         pass
 
-    def dotted_white_block(self):
+    def forward(self, x):
         pass
 
-    def purple_block(self):
+class En_De_3(nn.module):
+    def __init__(self):
+        super(En_De_3, self).__init__()
         pass
 
-    def En_1(self):
+    def forward(self, x):
         pass
 
-    def En_2(self):
+class En_De_4(nn.module):
+    def __init__(self):
+        super(En_De_4, self).__init__()
         pass
 
-    def En_3(self):
+    def forward(self, x):
         pass
 
-    def En_4(self):
-        pass
-
-    def forward(self, z):
-        pass
 
 
 if __name__ == "__main__":
