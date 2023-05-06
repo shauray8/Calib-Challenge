d74fd1532ea6861e0d458ca6d0afb0231226bde0
Shauray <shauray9@gmail.com>
2023-05-07 00:08:26 +0530

    addind testing studd

--- segnets/test.py
+++ segnets/test.py
@@ -17,9 +17,22 @@ import numbers
 import types
 import argparse
 
+from model import *
+
 imgs_dir = "E:\data\comma10k\comma10k\imgs"
+pretrained_model = "./pretrained/checkpoint.pkl"
 
 ids = [file for file in os.listdir(imgs_dir)]
 
-print(len(ids))
+model = Unet_square(4,4)
+with open(pretrained_model, 'rb') as pickle_file:
+    network_data = pickle.load(pickle_file)
 
+model.load_state_dict(network_data["state_dict"])
+main_epoch = network_data['epoch']
+
+print(ids[0])
+
+test = np.asarray(Image.open(ids[0]).resize((H//8,W//8)))
+img = model(test)
+print(img.shape)
